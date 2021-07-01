52507738eb40a8a7bb110b7c6061a47a77dc58ae
babinicz <babinicz@onet.pl>
2021-07-01 19:56:18 +0200

    dodałem popup

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Chef from "../images/1-svg.svg";
 import { Link } from "react-router-dom";
 import "./css/Banner.css";
+import Popup from "./Popup";
+import web from "../images/22a.jpg";
 
-const Banner = () => {
+const Banner = ({ title, img, p, em }) => {
+  const [buttonPopup, setButtonPopup] = useState(false);
+
   return (
     <>
       <div className="banner">
@@ -13,8 +17,18 @@ const Banner = () => {
           <p className="text">
             <i className="fa fa-quote-left"></i> ... vel Makłowicz
           </p>
-          <Link to="/services">Więcej</Link>
+          <Link onClick={() => setButtonPopup(true)}>Więcej</Link>
         </div>
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <h3>
+            „Jak się uderzy w stół, to nożyce się otworzą i to piwo co się
+            wylało trzeba wypić.“
+          </h3>
+          <br />
+          <img src={web} /> <br />
+          <p>Andrzej Lepper ✝</p> <br />
+          <em>https://quotepark.com/pl/autorzy/andrzej-lepper/</em>
+        </Popup>
       </div>
     </>
   );
